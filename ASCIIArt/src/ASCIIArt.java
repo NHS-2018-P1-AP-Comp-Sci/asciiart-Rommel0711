7513bf67f75263a548631560959751ee08572f98
s-liuxinl <s-liuxinl@53S50030498.it.bsd405.org>
2018-09-20 08:29:46 -0700

    Added some changes.

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -15,3 +15,43 @@
  */
 
 // YOUR CODE GOES HERE :)
+
+public class ASCIIArt{
+	
+	public static void printDash(){
+		String line1 = "\n---------------- ";
+		System.out.println(line1);
+	}
+	
+	public static void printTitle(){
+		System.out.println(" < AP CS Rocks > \n ");
+	}
+	
+	public static void printCow() {
+		System.out.println("\t\\   ^__^ ");
+		System.out.println("\t \\  (oo)\\________ ");
+		System.out.println("\t    (__)\\\t)\\/\\ ");
+		System.out.println("\t\t||----w | ");
+		System.out.println("\t\t||      |\n");
+	}
+	
+	public static void printSentence() {
+		System.out.println("A cow exclaiming their love for computer science. ");
+	}
+	
+	public static void printStarter(){
+		printDash();
+		printTitle();
+		printCow();
+		printSentence();
+	}
+	
+	public static void main(String[] args){
+		int num1 = 1;
+		while (num1 <= 3) {
+			System.out.println("\n"+num1);
+			printStarter();
+			num1 += 1;
+		}
+	}
+}
